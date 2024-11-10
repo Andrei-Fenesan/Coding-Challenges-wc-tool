dbb7d4fa705fe51eea842cac0cd9bdf15d59a5e4
Andrei Fenesan <andrei.fenesan.8x8.com>
2024-11-10 10:02:37 +0200

    Stage 2: Number of lines in a file

--- wcCommand/gowc.go
+++ wcCommand/gowc.go
@@ -21,6 +21,38 @@ func countNumberOfBytes(file *os.File) int {
 	return bytesCount
 }
 
+func numberOfLineBreaks(data []byte, size int) int {
+	lineBreaks := 0
+	for i := 0; i < size; i++ {
+		if data[i] == '\n' {
+			lineBreaks++
+		}
+	}
+	return lineBreaks
+}
+
+func countNumberOfLines(file *os.File) int {
+	reader := bufio.NewReader(file)
+	bytesBuffer := make([]byte, 4096)
+	lines := 0
+	for {
+		bytesRead, err := reader.Read(bytesBuffer)
+		if err == io.EOF {
+			break
+		}
+		lines += numberOfLineBreaks(bytesBuffer, bytesRead)
+	}
+	return lines
+}
+
+func isFlagValid(flag string) bool {
+	switch flag {
+	case "-c", "-l":
+		return true
+	}
+	return false
+}
+
 func main() {
 	cmdArgs := os.Args
 	if len(cmdArgs) != 3 {
@@ -29,13 +61,18 @@ func main() {
 	}
 	flag := cmdArgs[1]
 	filePath := cmdArgs[2]
-	if flag == "-c" {
+	if isFlagValid(flag) {
 		file, err := os.Open(filePath)
 		if err != nil {
 			fmt.Print("No file found: " + filePath)
 			return
 		}
 		defer file.Close()
-		fmt.Printf("%d", countNumberOfBytes(file))
+
+		if flag == "-c" {
+			fmt.Printf("%d %s", countNumberOfBytes(file), filePath)
+		} else if flag == "-l" {
+			fmt.Printf("%d %s\n", countNumberOfLines(file), filePath)
+		}
 	}
 }
--- wcCommand/gowc_test.go
+++ wcCommand/gowc_test.go
@@ -18,3 +18,15 @@ func TestFileByteCount(t *testing.T) {
 
 	assert.Equal(336747, countNumberOfBytes(file))
 }
+
+func TestFileLineCount(t *testing.T) {
+	assert := assert.New(t)
+
+	file, err := os.Open("test.txt")
+	if err != nil {
+		t.Fatalf("Cannot open file")
+	}
+	defer file.Close()
+
+	assert.Equal(8850, countNumberOfLines(file))
+}
